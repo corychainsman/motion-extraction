246e6e4fe327e87e7029546f0cd3407e771003f1
Cory Chainsman <1084726+corychainsman@users.noreply.github.com>
2025-09-08 08:59:24 -0400

    reconfigure deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/motion-extraction/' : '/',
+  base: '/motion-extraction/',
   build: {
-    outDir: '.',
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: './index.html'
